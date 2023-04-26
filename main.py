69a7f593a93d68af32522924df596943ab985844
Vitaliy <sprinter-100m@ukr.net>
2023-04-26 16:35:12 +0300

    study reminder

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import time
+import logging
+import config
+
 from aiogram import Bot, Dispatcher, executor, types
 
-TOKEN_API = "6051091753:AAElPDTxvI55ASpSjfzGrMk2zpMATwzQl4g"
-
-bot = Bot(TOKEN_API)
+bot = Bot(token=config.TOKEN)
 dp = Dispatcher(bot)
 
+MESSAGE = 'Програмировал ли ты сегодня, {}?'
 
-@dp.message_handler()
-async def eche(message: types.Message):
-    await message.answer(text=message.text)
+@dp.message_handler(commands=['start'])
+async def start_handler(message: types.Message):
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    user_full_name = message.from_user.full_name
+    logging.info(f'{user_id=} {user_full_name=} {time.asctime()}')
+
+    await message.reply(f'Привет, {user_full_name}')
+
+    for el in range(7):
+        time.sleep(60*60*24)
+        await bot.send_message(user_id, MESSAGE.format(user_name))
 
 
 if __name__ == '__main__':
